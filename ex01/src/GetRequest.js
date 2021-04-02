c5113f8e92dc12dea895b90756ec786a52b6fd89
amnazu <amna_zukic@hotmail.com>
2021-04-02 23:00:44 +0200

    Last day

---
+++ ex01/src/GetRequest.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class GetRequest extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { totalReactPackages: null };
+    }
+    render() {
+        const { totalReactPackages } = this.state;
+        return (
+            <div className="card text-center m-3">
+                <h5 className="cardheader">Simple GET Request</h5>
+                <div className="card-body">Total packages: {totalReactPackages}</div>
+            </div>
+        );
+    }
+    componentDidMount() {
+        axios.get('https://api.npms.io/v2/search?q=react')
+        .then(response => this.setState({ totalReactPackages: response.data.total}));
+    }
+}
+export default GetRequest;
